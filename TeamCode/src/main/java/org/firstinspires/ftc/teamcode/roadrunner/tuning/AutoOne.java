ab3aa1a3f21dcfc124b03813355229e2d1f39c8a
Flag7085 <roboticsflag7085@gmail.com>
2025-10-20 20:22:53 -0400

    Forward Auto Movement

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/tuning/AutoOne.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.roadrunner.tuning;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
+import org.firstinspires.ftc.teamcode.roadrunner.TankDrive;
+@Autonomous(name="Robot AutoOne")
+public final class AutoOne extends LinearOpMode{
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Pose2d beginPose = new Pose2d(0, 0, 0);
+        MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
+
+        waitForStart();
+        Actions.runBlocking(
+                drive.actionBuilder(beginPose)
+                        .splineTo(new Vector2d(30, 0), Math.toRadians(0))
+                        .build());
+
+        }
+}
