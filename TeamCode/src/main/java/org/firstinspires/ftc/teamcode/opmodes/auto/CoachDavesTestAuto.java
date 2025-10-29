05d7d707b0641bffd61d2e3b5f0c0dd70b61c882
David Karmol <dkarmol1@gmail.com>
2025-10-28 22:38:24 -0400

    Move Coach Dave's MeepMeep trajectory into CoachDave's auto test to verify RoadRunner tuning.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/CoachDavesTestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/CoachDavesTestAuto.java
@@ -1,10 +1,16 @@
 package org.firstinspires.ftc.teamcode.opmodes.auto;
 
+import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
+import org.firstinspires.ftc.teamcode.subsystem.IntakeSubsystem;
 
+@Autonomous(name = "Coach Dave's Test")
 public class CoachDavesTestAuto extends LinearOpMode {
     /**
      * Override this method and place your code here.
@@ -20,14 +26,55 @@ public class CoachDavesTestAuto extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
 
-        Pose2d beginPose = new Pose2d(0, 0, 0);
+        Pose2d beginPose = new Pose2d(-63, 30, Math.toRadians(180));
         MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
+        IntakeSubsystem intake = new IntakeSubsystem(hardwareMap, telemetry);
 
 
+        Action roadRunnerTrajectory = drive.actionBuilder(beginPose)
+
+                // Line up and shoot
+                .lineToXSplineHeading(-30, Math.toRadians(135))
+                .waitSeconds(1.0)
+
+                // Go pick up first line of Artifacts
+                .setReversed(true)
+                .splineToLinearHeading(new Pose2d(-12, 24, Math.toRadians(90)), Math.toRadians(0))
+                .setReversed(false)
+                .lineToY(52, (pose2dDual, posePath, v) -> 10)
+
+                // Line up and shoot
+                .setReversed(true)
+                .splineToLinearHeading(new Pose2d(-30, 30, Math.toRadians(135)),  Math.toRadians(180))
+                .waitSeconds(1.0)
+                .setReversed(false)
+
+                // Go pick up second line of Artifacts
+                .setReversed(true)
+                .splineTo(new Vector2d(0, 24), Math.toRadians(0))
+                .splineToSplineHeading(new Pose2d(12, 30, Math.toRadians(90)), Math.toRadians(90))
+                .setReversed(false)
+                .lineToY(52, (pose2dDual, posePath, v) -> 10)
+
+                // Line up and shoot
+                .setReversed(true)
+                .splineToLinearHeading(new Pose2d(-30, 30, Math.toRadians(135)),  Math.toRadians(180))
+                .waitSeconds(1.0)
+                .setReversed(false)
+
+                // Go park to the side near goal
+                .setTangent(Math.toRadians(60))
+                .splineToLinearHeading(new Pose2d(-24, 56, Math.toRadians(180)), Math.toRadians(90))
+                .waitSeconds(0.5)
+
+                .build();
 
         waitForStart();
         if (isStopRequested()) {
             return;
         }
+
+        Actions.runBlocking(roadRunnerTrajectory);
+
     }
 }
